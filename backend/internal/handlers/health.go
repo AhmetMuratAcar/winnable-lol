6839ea3449c01025ef7914031f66c9c5a1bf3803
AhmetMuratAcar <muratacar.dev@gmail.com>
2025-05-24 12:55:25 -0700

    update: health endpoint to only allow GET requests

--- backend/internal/handlers/health.go
+++ backend/internal/handlers/health.go
@@ -7,7 +7,13 @@ import (
 type HealthHandler struct{}
 
 func (h *HealthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		http.Error(w, "405 Method Not Allowed: only GET is supported", http.StatusMethodNotAllowed)
+		return
+	}
+
 	// return ok
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("ok"))
+	w.Write([]byte("lookin good\n"))
 }
