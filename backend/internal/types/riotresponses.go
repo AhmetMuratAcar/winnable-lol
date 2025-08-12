ad8f086a1362eb267345811e6ddd0c5aad429d99
AhmetMuratAcar <muratacar.dev@gmail.com>
2025-08-11 19:27:38 -0700

    add league match, participant, and rank types

--- backend/internal/types/riotresponses.go
+++ backend/internal/types/riotresponses.go
@@ -19,15 +19,54 @@ type MasteryData struct {
 	ChampionsPlayed    int               `json:"championsPlayed"`
 }
 
-// TODO: figure out exactly what to grab from riot's API for each match
-type LeagueMatch struct{}
+type LeagueMatch struct {
+	EndOfGameResult    string                   `json:"endOfGameResult"`
+	GameDuration       int                      `json:"gameDuration"`
+	GameStartTimestamp int                      `json:"gameStartTimeStamp"`
+	MatchID            string                   `json:"matchId"`
+	ParticipantPUUIDs  []string                 `json:"participantPUUIDs"`
+	Participants       []LeagueMatchParticipant `json:"participants"`
+	QueueId            int                      `json:"queueId"`
+	Bans               []int                    `json:"bans"`
+}
+
+type LeagueMatchParticipant struct {
+	Assists                     int    `json:"assists"`
+	ChampionID                  int    `json:"championId"`
+	ChampLevel                  int    `json:"champLevel"`
+	Deaths                      int    `json:"deaths"`
+	GoldEarned                  int    `json:"goldEarned"`
+	Items                       []int  `json:"items"`
+	Kills                       int    `json:"kills"`
+	ProfileIconID               int    `json:"profileIcon"`
+	PUUID                       string `json:"puuid"`
+	RiotIDGameName              string `json:"riotIdGameName"`
+	RiotIDTagline               string `json:"riotIdTagline"`
+	Summoner1ID                 int    `json:"summoner1Id"`
+	Summoner2ID                 int    `json:"summoner2Id"`
+	SummonerLevel               int    `json:"summonerLevel"`
+	TeamPosition                string `json:"teamPosition"`
+	TotalDamageDealtToChampions int    `json:"totalDamageDealtToChampions"`
+	TotalMinionsKilled          int    `json:"totalMinionsKilled"`
+	VisionScore                 int    `json:"visionScore"`
+}
+
+type LeagueRank struct {
+	QueueType    string `json:"queueType"`
+	Tier         string `json:"tier"`
+	Rank         string `json:"rank"`
+	LeaguePoints int    `json:"leaguePoints"`
+	Wins         int    `json:"wins"`
+	Losses       int    `json:"losses"`
+}
+
 type LeagueProfilePage struct {
-	ProfileIconId int           `json:"profileIconId"`
+	ProfileIconID int           `json:"profileIconId"`
 	GameName      string        `json:"gameName"`
 	TagLine       string        `json:"tagLine"`
 	Region        string        `json:"region"`
 	Level         int           `json:"summonerLevel"`
-	Rank          string        `json:"rank"`
+	Ranks         []LeagueRank  `json:"ranks"`
 	MasteryData   MasteryData   `json:"masteryData"`
 	LastGames     []LeagueMatch `json:"lastGames"`
 }
