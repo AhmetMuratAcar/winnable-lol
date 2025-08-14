d2af8b510ce3fb8b1f82ab5c29e807b28dc80a2b
AhmetMuratAcar <muratacar.dev@gmail.com>
2025-08-14 16:52:16 -0700

    add even more db funcs to write

--- backend/internal/utils/dbutils.go
+++ backend/internal/utils/dbutils.go
@@ -28,3 +28,32 @@ func GetMasteries(ctx context.Context, pool *pgxpool.Pool, PUUID string) ([]type
 	var championMasteries []types.ChampionMastery
 	return championMasteries, nil
 }
+
+// GetMatchIDs queries matches table for given user's past Match IDs
+// Example:
+//
+//	matchIDs, err := utils.GetMatchIDs(ctx, h.pool, PUUID)
+func GetMatchIDs(ctx context.Context, pool *pgxpool.Pool, PUUID string) ([]string, error) {
+	// TODO: actually query DB for user matches
+	var matchIDs []string
+	return matchIDs, nil
+}
+
+// GetMatchDataByIDs populates a map of matchID: types.LeagueMatch based on
+// if the matchID is cached in the matches table
+// Example:
+//
+//	err := utils.GetMatchDataByIDs(ctx, h.pool, matchIDs, &matchDataMap)
+func GetMatchDataByIDs(ctx context.Context, pool *pgxpool.Pool, matchIDs []string, matchMap *map[string]*types.LeagueMatch) error {
+	// TODO actually loop over matchMap and check cache status
+	// Remember if the matchID isnt cached, the value in the map should be set to nil
+	return nil
+}
+
+// AddMatchData updates the matches table with newly fetched matchData
+// Example:
+//
+//	err := utils.AddMatchData(ctx, h.pool, toAdd)
+func AddMatchData(ctx context.Context, pool *pgxpool.Pool, matchData []types.LeagueMatch) error {
+	return nil
+}
