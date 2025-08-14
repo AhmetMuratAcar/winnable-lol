ee1739b91bd0634644541162b2801db93ae0f26f
AhmetMuratAcar <muratacar.dev@gmail.com>
2025-08-14 16:49:09 -0700

    add getsummonermatchids and start getmatchdata

--- backend/internal/riot/client.go
+++ backend/internal/riot/client.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
@@ -29,13 +30,13 @@ func NewClientWithHTTPClient(httpClient *http.Client) *RiotClient {
 	return &RiotClient{
 		httpClient: httpClient,
 		apiKey:     os.Getenv("RIOT_API_KEY"),
-		baseURL:    "api.riotgames.com/",
+		baseURL:    "api.riotgames.com",
 	}
 }
 
 func (c *RiotClient) GetSummonerPUUID(reqBody types.RequestBody) (puuid string, err error) {
 	// TODO: actually route to nearest server instead of defaulting all to americas
-	baseEndpoint := "https://americas." + c.baseURL + "riot/account/v1/accounts/by-riot-id"
+	baseEndpoint := "https://americas." + c.baseURL + "/riot/account/v1/accounts/by-riot-id"
 	endpoint := fmt.Sprintf(
 		"%s/%s/%s",
 		baseEndpoint,
@@ -57,11 +58,14 @@ func (c *RiotClient) GetSummonerPUUID(reqBody types.RequestBody) (puuid string, 
 
 	if res.StatusCode != http.StatusOK {
 		bodyBytes, _ := io.ReadAll(res.Body)
-		return "", fmt.Errorf(
-				"non-200 response: %s\n%s", 
-				res.Status, 
-				string(bodyBytes),
-			)
+		err := fmt.Errorf(
+			"non-200 response: %s\n%s",
+			res.Status,
+			string(bodyBytes),
+		)
+
+		log.Printf("RIOT API ERROR: %v", err)
+		return "", err
 	}
 
 	var result types.AccountResponse
@@ -74,7 +78,7 @@ func (c *RiotClient) GetSummonerPUUID(reqBody types.RequestBody) (puuid string, 
 
 func (c *RiotClient) GetSummonerMastery(region, puuid string) ([]types.ChampionMastery, error) {
 	region = strings.ToLower(region)
-	baseEndpoint := "https://" + region + ".api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid"
+	baseEndpoint := "https://" + region + "." + c.baseURL + "/lol/champion-mastery/v4/champion-masteries/by-puuid"
 	endpoint := fmt.Sprintf(
 		"%s/%s",
 		baseEndpoint,
@@ -95,11 +99,14 @@ func (c *RiotClient) GetSummonerMastery(region, puuid string) ([]types.ChampionM
 
 	if res.StatusCode != http.StatusOK {
 		bodyBytes, _ := io.ReadAll(res.Body)
-		return nil, fmt.Errorf(
-				"non-200 response: %s\n%s", 
-				res.Status, 
-				string(bodyBytes),
-			)
+		err := fmt.Errorf(
+			"non-200 response: %s\n%s",
+			res.Status,
+			string(bodyBytes),
+		)
+
+		log.Printf("RIOT API ERROR: %v", err)
+		return nil, err
 	}
 
 	var championMasteries []types.ChampionMastery
@@ -108,4 +115,54 @@ func (c *RiotClient) GetSummonerMastery(region, puuid string) ([]types.ChampionM
 	}
 
 	return championMasteries, nil
+}
+
+func (c *RiotClient) GetSummonerMatchIDs(puuid string, start int) ([]string, error) {
+	// TODO: actually route to nearest server instead of defaulting all to americas
+	baseEndpoint := "https://americas." + c.baseURL + "/lol/match/v5/matches/by-puuid"
+	count := "20"
+	startStr := fmt.Sprintf("%d", start)
+	endpoint := fmt.Sprintf(
+		"%s/%s/ids?start=%s&count=%s",
+		baseEndpoint,
+		puuid,
+		startStr,
+		count,
+	)
+
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating GetSummonerMatchIDs request: %w", err)
+	}
+	req.Header.Set("X-Riot-Token", c.apiKey)
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("GetSummonerMatchIDs API request failed: %w", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		bodyBytes, _ := io.ReadAll(res.Body)
+		err := fmt.Errorf(
+			"non-200 response: %s\n%s",
+			res.Status,
+			string(bodyBytes),
+		)
+
+		log.Printf("RIOT API ERROR: %v", err)
+		return nil, err
+	}
+
+	var result []string
+	if err := json.NewDecoder(res.Body).Decode(&result); err != nil {
+		return nil, fmt.Errorf("error decoding JSON: %w", err)
+	}
+
+	return result, nil
+}
+
+func (c *RiotClient) GetMatchData(matchID string) (types.LeagueMatch, error) {
+	var result types.LeagueMatch
+	return result, nil 
 }
