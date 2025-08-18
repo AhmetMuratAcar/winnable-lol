2dd47442093d4badfa13287dd13a686d424913bf
AhmetMuratAcar <muratacar.dev@gmail.com>
2025-08-18 01:27:52 -0700

    add RawMatchResponse to LeagueMatch converter

---
+++ backend/internal/utils/riot.go
@@ -0,0 +1,62 @@
+package utils
+
+import "winnable/internal/types"
+
+func ToLeagueMatch(raw types.RawMatchResponse) types.LeagueMatch {
+	res := types.LeagueMatch{
+		EndOfGameResult:    raw.Info.EndOfGameResult,
+		GameDuration:       raw.Info.GameDuration,
+		GameStartTimestamp: raw.Info.GameStartTimestamp,
+		GameVersion:        raw.Info.GameVersion,
+		MatchID:            raw.Metadata.MatchID,
+		ParticipantPUUIDs:  raw.Metadata.Participants,
+		QueueId:            raw.Info.QueueID,
+	}
+
+	for _, p := range raw.Info.Participants {
+		curr := types.LeagueMatchParticipant{
+			Assists:                     p.Assists,
+			ChampionID:                  p.ChampionID,
+			ChampLevel:                  p.ChampLevel,
+			Deaths:                      p.Deaths,
+			GoldEarned:                  p.GoldEarned,
+			Kills:                       p.Kills,
+			ProfileIconID:               p.ProfileIcon,
+			PUUID:                       p.Puuid,
+			RiotIDGameName:              p.RiotIDGameName,
+			RiotIDTagline:               p.RiotIDTagline,
+			Summoner1ID:                 p.Summoner1ID,
+			Summoner2ID:                 p.Summoner2ID,
+			SummonerLevel:               p.SummonerLevel,
+			TeamPosition:                p.TeamPosition,
+			TotalDamageDealtToChampions: p.TotalDamageDealtToChampions,
+			TotalMinionsKilled:          p.TotalMinionsKilled,
+			VisionScore:                 p.VisionScore,
+		}
+
+		curr.Items = []int{
+			p.Item0,
+			p.Item1,
+			p.Item2,
+			p.Item3,
+			p.Item4,
+			p.Item5,
+		}
+
+		res.Participants = append(res.Participants, curr)
+	}
+
+	for i, t := range raw.Info.Teams {
+		for j, b := range t.Bans {
+			res.Bans[i][j] = b.ChampionID
+		}
+	}
+
+	if raw.Info.Teams[0].Win {
+		res.WinningTeam = 0
+	} else {
+		res.WinningTeam = 1
+	}
+
+	return res
+}
