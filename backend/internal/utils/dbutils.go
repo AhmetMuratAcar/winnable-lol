48bf61d16aad93d68f54da35bf1a4aeae27f5b12
AhmetMuratAcar <muratacar.dev@gmail.com>
2025-08-22 08:07:21 -0700

    clean up

--- backend/internal/utils/dbutils.go
+++ backend/internal/utils/dbutils.go
@@ -12,31 +12,9 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-// MarkSummonerStale marks a summoner as stale by changing the updated_at column of the
-// summoners table to 24 hours before the current time.
-func MarkSummonerStale(ctx context.Context, pool *pgxpool.Pool, puuid string) error {
-	const query = `
-		UPDATE summoners
-		SET updated_at = now() - interval '24 hours'
-		WHERE puuid = $1;
-	`
-
-	cmdTag, err := pool.Exec(ctx, query, puuid)
-	if err != nil {
-		return fmt.Errorf("failed to mark summoner stale: %w", err)
-	}
-
-	if cmdTag.RowsAffected() == 0 {
-		return fmt.Errorf("no summoner found with puuid: %s", puuid)
-	}
-
-	return nil
-}
+/* ------------------------ SELECT Queries ------------------------ */
 
 // GetPUUID queries summoners table for given user's PUUID
-// Example:
-//
-//	cacheCheck, err := utils.GetPUUID(ctx, h.pool, req)
 func GetPUUID(ctx context.Context, pool *pgxpool.Pool, userInfo types.RequestBody) (types.PUUIDCacheCheck, error) {
 	var puuid string
 	var updatedAt time.Time
@@ -76,10 +54,7 @@ func GetPUUID(ctx context.Context, pool *pgxpool.Pool, userInfo types.RequestBod
 	}, nil
 }
 
-// GetMasteries queries masteries table for given user's champion masteries
-// Example:
-//
-//	championMasteries, err := utils.GetMasteries(ctx, h.pool, PUUID)
+// GetMasteries queries champion_masteries table for given user's champion masteries
 func GetMasteries(ctx context.Context, pool *pgxpool.Pool, PUUID string) ([]types.ChampionMastery, error) {
 	const query = `
 		SELECT champion_id, champion_level, champion_points
@@ -115,10 +90,7 @@ func GetMasteries(ctx context.Context, pool *pgxpool.Pool, PUUID string) ([]type
 	return championMasteries, nil
 }
 
-// GetMatchIDs queries matches table for given user's past 20 Match IDs
-// Example:
-//
-//	matchIDs, err := utils.GetMatchIDs(ctx, h.pool, PUUID)
+// GetMatchIDs queries match_participants table for given user's past 20 Match IDs
 func GetMatchIDs(ctx context.Context, pool *pgxpool.Pool, PUUID string) ([]string, error) {
 	const query = `
 		SELECT match_id
@@ -151,19 +123,38 @@ func GetMatchIDs(ctx context.Context, pool *pgxpool.Pool, PUUID string) ([]strin
 
 // GetMatchDataByIDs populates a map of matchID: types.LeagueMatch based on
 // if the matchID is cached in the matches table
-// Example:
-//
-//	err := utils.GetMatchDataByIDs(ctx, h.pool, matchIDs, &matchDataMap)
 func GetMatchDataByIDs(ctx context.Context, pool *pgxpool.Pool, matchIDs []string, matchMap *map[string]*types.LeagueMatch) error {
 	// TODO actually loop over matchMap and check cache status
 	// Remember if the matchID isnt cached, the value in the map should be set to nil
 	return nil
 }
 
+/* ------------------------ INSERT Queries ------------------------ */
+
 // AddMatchData updates the matches table with newly fetched matchData
-// Example:
-//
-//	err := utils.AddMatchData(ctx, h.pool, toAdd)
 func AddMatchData(ctx context.Context, pool *pgxpool.Pool, matchData []types.LeagueMatch) error {
+	return nil
+}
+
+/* ------------------------ UPDATE Queries ------------------------ */
+
+// MarkSummonerStale marks a summoner as stale by changing the updated_at column of the
+// summoners table to 24 hours before the current time.
+func MarkSummonerStale(ctx context.Context, pool *pgxpool.Pool, puuid string) error {
+	const query = `
+		UPDATE summoners
+		SET updated_at = now() - interval '24 hours'
+		WHERE puuid = $1;
+	`
+
+	cmdTag, err := pool.Exec(ctx, query, puuid)
+	if err != nil {
+		return fmt.Errorf("failed to mark summoner stale: %w", err)
+	}
+
+	if cmdTag.RowsAffected() == 0 {
+		return fmt.Errorf("no summoner found with puuid: %s", puuid)
+	}
+
 	return nil
 }
