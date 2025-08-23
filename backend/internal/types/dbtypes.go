c06fa9de76ab2b4e4dd70628ea1a2ece5b5372a8
AhmetMuratAcar <muratacar.dev@gmail.com>
2025-08-23 07:53:42 -0700

    add DB table row structs

--- backend/internal/types/dbtypes.go
+++ backend/internal/types/dbtypes.go
@@ -1,5 +1,7 @@
 package types
 
+import "time"
+
 type PUUIDCacheCheck struct {
 	Found bool
 	PUUID string
@@ -13,4 +15,71 @@ type CachedProfileCheckList struct {
 	Ranks       bool
 	Masteries   bool
 	Matches     bool
+}
+
+type MatchRow struct {
+	MatchID         string
+	EndOfGameResult string
+	GameDurationSec int
+	GameStart       time.Time
+	GameVersion     string
+	QueueID         int
+	WinningTeam     int
+	BansBlue        []int
+	BansRed         []int
+}
+
+type MatchParticipantRow struct {
+	MatchID              string
+	PUUID                string
+	ParticipantIndex     int
+	Team                 int
+	ChampionID           int
+	ChampLevel           int
+	Kills                int
+	Deaths               int
+	Assists              int
+	GoldEarned           int
+	TotalDamageToChamps  int
+	TotalMinionsKilled   int
+	VisionScore          int
+	Items                []int
+	Summoner1ID          int
+	Summoner2ID          int
+	TeamPosition         string
+	RiotIDGameName       string
+	RiotIDTagLine        string
+	SummonerLevelAtMatch int
+	ProfileIconAtMatch   int
+}
+
+type SummonerRow struct {
+	PUUID              string
+	Region             string
+	GameName           string
+	TagLine            string
+	ProfileIconID      int
+	SummonerLevel      int
+	TotalMastery       int
+	TotalMasteryPoints int
+	ChampionsPlayed    int
+	CreatedAt          time.Time
+	UpdatedAt          time.Time
+}
+
+type RankRow struct {
+	PUUID        string
+	QueueType    string
+	Tier         string
+	Division     string
+	LeaguePoints int
+	Wins         int
+	Losses       int
+}
+
+type ChampionMasteryRow struct {
+	PUUID          string
+	ChampionID     int
+	ChampionLevel  int
+	ChampionPoints int
 }
