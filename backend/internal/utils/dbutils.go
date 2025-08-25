474c750e222b778d2b14964b978991cd04b7318e
AhmetMuratAcar <muratacar.dev@gmail.com>
2025-08-25 07:16:02 -0700

    add UpdateSummonersAll

--- backend/internal/utils/dbutils.go
+++ backend/internal/utils/dbutils.go
@@ -24,6 +24,26 @@ func SyncProfileData(
 ) error {
 	if cacheCheck.Found && !cacheCheck.IsPopulated {
 		// Update summoners table and add everything else
+		var sr = []types.SummonerRow{{
+			PUUID:              userProfile.PUUID,
+			Region:             userProfile.Region,
+			GameName:           userProfile.GameName,
+			TagLine:            userProfile.TagLine,
+			ProfileIconID:      userProfile.ProfileIconID,
+			SummonerLevel:      userProfile.Level,
+			TotalMastery:       userProfile.MasteryData.TotalMastery,
+			TotalMasteryPoints: userProfile.MasteryData.TotalMasteryPoints,
+			ChampionsPlayed:    userProfile.MasteryData.ChampionsPlayed,
+			UpdatedAt:          time.Now(),
+			IsPopulated:        true,
+		}}
+		if err := UpdateSummonersAll(ctx, pool, sr); err != nil {
+			return fmt.Errorf("error calling UpdateSummonersAll: %w", err)
+		}
+
+		// add masteries
+
+		// add ranks
 
 	} else if cacheCheck.Found && cacheCheck.Stale {
 		// Update all tables
@@ -225,6 +245,62 @@ func AddNewSummoners(ctx context.Context, pool *pgxpool.Pool, rows []types.Summo
 }
 
 /* ------------------------ UPDATE Queries ------------------------ */
+
+// UpdateSummonersAll updates all contents of a summoners' table row for each row given in rows
+func UpdateSummonersAll(ctx context.Context, pool *pgxpool.Pool, rows []types.SummonerRow) error {
+	if len(rows) == 0 {
+		return nil
+	}
+
+	const query = `
+		UPDATE summoners
+		SET
+			region               = $2,
+			game_name            = $3,
+			tag_line             = $4,
+			profile_icon_id      = $5,
+			summoner_level       = $6,
+			total_mastery        = $7,
+			total_mastery_points = $8,
+			champions_played     = $9,
+			updated_at           = $10,
+			is_populated         = $11
+		WHERE puuid = $1
+	`
+
+	batch := &pgx.Batch{}
+	for _, r := range rows {
+		if r.UpdatedAt.IsZero() {
+			r.UpdatedAt = time.Now()
+		}
+
+		batch.Queue(
+			query,
+			r.PUUID,
+			r.Region,
+			r.GameName,
+			r.TagLine,
+			r.ProfileIconID,
+			r.SummonerLevel,
+			r.TotalMastery,
+			r.TotalMasteryPoints,
+			r.ChampionsPlayed,
+			r.UpdatedAt,
+			r.IsPopulated,
+		)
+	}
+
+	br := pool.SendBatch(ctx, batch)
+	defer br.Close()
+
+	for range rows {
+		if _, err := br.Exec(); err != nil {
+			return fmt.Errorf("update summoner failed: %w", err)
+		}
+	}
+
+	return nil
+}
 
 // MarkSummonerStale marks a summoner as stale by changing the updated_at column of the
 // summoners table to 24 hours before the current time.
