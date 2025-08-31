891261102e5c395caf74d9c9ae7ea24d4e27c754
AhmetMuratAcar <muratacar.dev@gmail.com>
2025-08-31 10:30:59 -0700

    spacing

--- frontend/src/app/dev/slow-page/page.jsx
+++ frontend/src/app/dev/slow-page/page.jsx
@@ -6,14 +6,14 @@ export default async function SlowPage() {
 
   return (
     <main className="min-h-svh flex flex-col">
-        <Header />
-        <div className="flex flex-col flex-grow items-center justify-center px-4 py-6 text-center">
-          <h1 className="text-3xl font-bold">Slow Page Loaded</h1>
-          <p className="text-gray-500 mt-4">
-              This page delayed on purpose so you could see the global loading spinner.
-          </p>
-        </div>
-        <Footer />
+      <Header />
+      <div className="flex flex-col flex-grow items-center justify-center px-4 py-6 text-center">
+        <h1 className="text-3xl font-bold">Slow Page Loaded</h1>
+        <p className="text-gray-500 mt-4">
+            This page delayed on purpose so you could see the global loading spinner.
+        </p>
+      </div>
+      <Footer />
     </main>
   );
 }
