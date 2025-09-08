1ceec976fcaa8f407bbe8cb59225bd0a2caacc39
AhmetMuratAcar <muratacar.dev@gmail.com>
2025-09-07 17:09:50 -0700

    on hover: cursor pointer

--- frontend/src/app/components/LeagueProfile/RefreshButton.jsx
+++ frontend/src/app/components/LeagueProfile/RefreshButton.jsx
@@ -3,7 +3,7 @@ export default function RefreshButton() {
   return (
     <button
       onClick={console.log("Refreshing profile...")}
-      className="mt-4 px-3 py-2 rounded-md bg-[var(--pastel-red)] text-white hover:opacity-80"
+      className="mt-4 px-3 py-2 rounded-md bg-[var(--pastel-red)] text-white hover:opacity-80 hover:cursor-pointer"
     >
       Update
     </button>
