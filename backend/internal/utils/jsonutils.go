11f7d494f068ffacef492ea36d23895ebf21855a
AhmetMuratAcar <muratacar.dev@gmail.com>
2025-09-28 15:59:46 -0700

    add WriteMasteryToFile helper

--- backend/internal/utils/jsonutils.go
+++ backend/internal/utils/jsonutils.go
@@ -10,19 +10,37 @@ import (
 )
 
 func WriteProfileToFile(data types.LeagueProfilePage, riotID string) error {
-	path := fmt.Sprintf("./devfiles/%s_LoLProfile.JSON", riotID)
+	path := fmt.Sprintf("./devfiles/profiles/%s_LoLProfile.JSON", riotID)
 	f, err := os.Create(path)
 	if err != nil {
-		return fmt.Errorf("failed to create file: %w", err)
+		return fmt.Errorf("failed to create profile file: %w", err)
 	}
 	defer f.Close()
 
 	enc := json.NewEncoder(f)
 	enc.SetIndent("", "  ")
 	if err := enc.Encode(data); err != nil {
-		return fmt.Errorf("failed to write JSON: %w", err)
+		return fmt.Errorf("failed to write profile JSON: %w", err)
 	}
 
 	log.Printf("wrote %s profile to %s", riotID, path)
+	return nil
+}
+
+func WriteMasteryToFile(data types.MasteryData, riotID string) error {
+	path := fmt.Sprintf("./devfiles/masteries/%s_MasteryData.JSON", riotID)
+	f, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("failed to create mastery file: %w", err)
+	}
+	defer f.Close()
+
+	enc := json.NewEncoder(f)
+	enc.SetIndent("", "  ")
+	if err := enc.Encode(data); err != nil {
+		return fmt.Errorf("failed to write mastery JSON: %w", err)
+	}
+
+	log.Printf("wrote %s mastery to %s", riotID, path)
 	return nil
 }
